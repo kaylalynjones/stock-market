6b83cd461d59099f4363f79cf2811571ab0ca108
Kayla Jones <kaylalyne424@gmail.com>
2014-07-21 13:49:06 -0500

    instance method: added an add prototype to Portfolio

--- app/models/portfolio.js
+++ app/models/portfolio.js
@@ -6,4 +6,15 @@ function Portfolio(name){
 
 }
 
+Portfolio.prototype.add = function(stock){
+  for(var i = 0; i < this.stocks.length; i++){
+    if(this.stocks[i].symbol === stock.symbol){
+      this.stocks[i].count += stock.count;
+      return;
+    }
+  }
+
+  this.stocks.push(stock);
+};
+
 module.exports = Portfolio;
--- test/unit/portfolio.js
+++ test/unit/portfolio.js
@@ -5,6 +5,7 @@
 
 var expect = require('chai').expect;
 var Portfolio = require('../../app/models/portfolio');
+var Stock = require('../../app/models/stock');
 
 describe('Portfolio', function(){
   describe('constructor', function(){
@@ -17,4 +18,17 @@ describe('Portfolio', function(){
     });
   });
 
+  describe('#add', function(){
+    it('should stocks to portfolios', function(){
+      var tech = new Portfolio('tech');
+      tech.add( new Stock('aapl', 50));
+      tech.add( new Stock('amzn', 30));
+      tech.add( new Stock('aapl', 25));
+
+      expect(tech.stocks).to.have.length(2);
+      expect(tech.stocks[0].count).to.equal(75);
+      expect(tech.stocks[1]).to.be.instanceof(Stock);
+
+    });
+  });
 });
